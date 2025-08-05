eb6ce6e6d6335798ea9ddbf42531f467d4b54b62
vyshnavvijayan1131 <vyshnavvijayan2003@gmail.com>
2025-08-05 11:06:24 +0000

    Updated app text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('🚀 Hello from Jenkins CI/CD Pipeline with Docker!\n');
+  res.end('HI MALUUUU!\n');
 });
 server.listen(8080, () => {
   console.log('Server running on port 8080');
